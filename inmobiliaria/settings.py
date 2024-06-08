ab7e032679e1a295d13f5d3606d97784ce4fbf18
felipe_farfal <efe.farfal@gmail.com>
2024-06-08 11:59:57 -0400

    allowed hosts

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['*', 'proyecto-portal-inmobiliario-em59.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://proyecto-portal-inmobiliario-em59.onrender.com']
 
 AUTH_USER_MODEL = 'web.Usuario'
 
